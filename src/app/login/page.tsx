53e931386cef68e9a113394402aa023ed9e093ca
hiteshkb23 <hiteshkb2003@gmail.com>
2025-08-26 01:50:52 +0530

    removing borderwhite

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -125,7 +125,7 @@ export default function LoginPage(){
                                     id="remember-me"
                                     name="remember-me"
                                     type="checkbox"
-                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded bg-white/10 border-white/20"
+                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded bg-white/10"
                                 />
                                 <label htmlFor="remember-me" className="ml-2 text-gray-300">
                                     Remember me
