5e9823e4e64ac9d60081557d1dc8a450cdf1991c
hiteshkb23 <hiteshkb2003@gmail.com>
2025-08-25 20:17:22 +0530

    added middle ware and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import {getDatafromToken} from "@/helpers/getDatafromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/model/userModel";
+import {connect} from "@/dbConfig/dbConfig"
+
+connect();
+
+export async function GET(request:NextRequest){
+    try{
+        const userId = await getDatafromToken(request);
+        const user = await User.findOne({_id:userId}).select("-password");
+        return NextResponse.json({
+            message: "user found",
+            data: user
+        })
+    }catch(error :any){
+        return NextResponse.json({error: error.message},{status :400});
+    }
+}
